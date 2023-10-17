0b29b4a707e72ade233473c01fbc76deeec27f89
giovanasilveiracardoso <giovanasilveiracardoso@gmail.com>
2023-10-17 15:12:17 -0300

    fix - Ajustando build path do Java

--- src/main/com/amf/crm/data/repository/LoanRepository.java
+++
@@ -1,9 +0,0 @@
-package com.amf.crm.data.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.amf.crm.data.entity.Loan;
-
-public interface LoanRepository extends JpaRepository<Loan, Long> {
-
-}
